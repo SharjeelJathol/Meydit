eb6347d8cb4e24b762593742e55555597f371c6a
Sharjeel Ahmad <sharjeeljathol999@gmail.com>
2023-04-30 08:22:43 +0500

    Record insertion of hasMany relationship
    Another way to create/insert a new record in database is as
    const job=new Job()
    job.first_name=body.first_name;
    job.last_name=body.last_name;
    job.clothing=body.clothing;
    job.email=body.email;
    job.address=body.address;
    job.postal_code=body.postal_code;
    job.state=body.state;
    job.description=body.description;
    job.budget=body.budget;
    job.count=0
    const response=await job.save();
    
    It is important to understand the underlying concept behind relationship in database by Adonisjs. That how dependant tables are filled by the higher order tables having relation between them. But the key concept is that a table having hasMany, ManyToMany relationoship is responsible for insertion of new record by first accessing the instance of that record which is going to be associated with the new record of an independent table which is deriving a key from the higher order table. Database Model essential are also to be remembered which will be explained by the next commit about Models.

--- api/app/Controllers/Http/JobsController.ts
+++ api/app/Controllers/Http/JobsController.ts
@@ -1,21 +1,51 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Database from '@ioc:Adonis/Lucid/Database'
 import Job from 'App/Models/Job'
-import Jobs from 'Database/migrations/1682617018790_create_jobs_tables'
 
 export default class JobsController {
   public async index(ctx: HttpContextContract) {
-    // const taskDatabse=await Database.from('Job').where('id', 1).select('*')
-    // const taskModel=await Job.query().where('id', 1).select('*')
+    const taskModel=await Job.query().where('id', ctx.params.id).select('*').first()
+    ctx.response.send(taskModel)
   }
 
   public async create({}: HttpContextContract) {}
 
   public async store(ctx: HttpContextContract) {
-    const job=new Job()
+
+    let body=ctx.request.body()
+    console.log('body:', body)
+    const images = ctx.request.files('samples')
+    let links:Array<string>=[];
+
+    const job= await Job.create({
+      first_name:body.first_name,
+      last_name:body.last_name,
+      clothing:body.clothing,
+      email:body.email,
+      address:body.address,
+      postal_code:body.postal_code,
+      state:body.state,
+      description:body.description,
+      budget:body.budget,
+      count:0,
+    });
+
+    for (let image of images) {
+      await image.moveToDisk('./')
+      const fileName = image.fileName;
+      if(fileName){
+        links.push(fileName)
+        console.log(fileName)
+        console.log(await job.related('samples').create({file:fileName}))
+      }
+    }
+    return {body: ctx.request.body()}
   }
 
-  public async show(ctx: HttpContextContract) {}
+  public async show(ctx: HttpContextContract) {
+    const taskModel=await Job.findMany([])
+    console.log(taskModel)
+    ctx.response.send({jobs:taskModel})
+  }
 
   public async edit({}: HttpContextContract) {}
 
