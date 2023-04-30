bbea50b74429525382bd6a87a370fcf5d8a97ea3
Sharjeel Ahmad <sharjeeljathol999@gmail.com>
2023-04-30 14:07:33 +0500

    Homepage
    Containing only Navbar and JobsAvalable components.

---
+++ frontend/src/components/Hompage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './Navbar';
+import JobsAvailable from './JobsAvailable';
+export default function Homepage() {
+  return (
+    <>
+      <Navbar/>
+      <JobsAvailable/>
+    </>
+  )
+}
