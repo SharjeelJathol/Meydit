0c65d50696af5ff186f4d94aa8b8dacb15edd614
Sharjeel Ahmad <sharjeeljathol999@gmail.com>
2023-05-06 12:17:07 +0500

    Extras

---
+++ api/README.md
@@ -0,0 +1,5 @@
+
+npm i
+node ace migration:rollback --batch 0
+node ace migration:run
+node ace serve --watch
--- api/start/routes.ts
+++ api/start/routes.ts
@@ -18,7 +18,6 @@
 |
 */
 
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async () => {
